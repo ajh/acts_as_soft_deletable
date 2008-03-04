e6bb9f204ec96c48a5a19f83ee753ffe2b9cfa94
(no author) <(no author)@bb26965d-a405-0410-8ce8-d62df5cd24e9>
2008-03-04 01:42:28 +0000

    improve fixtures and tests
    git-svn-id: http://llama/svn/trunk/ruby/acts_as_soft_deletable@221 bb26965d-a405-0410-8ce8-d62df5cd24e9

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,16 +1,33 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 class BaseTest < Test::Unit::TestCase
-  def test_destroy_creates_a_deleted_model
+  def test_destroy_should_create_a_deleted_model
     artist = Artist.find_by_name('Chick Corea')
     artist.destroy
     assert_nil Artist.find_by_name('Chick Corea')
 
     deleted = Artist::Deleted.find_by_name('Chick Corea')
-    assert_equal artist.attributes, deleted.attributes.reject{|k,v| k == 'deleted_at'}
-
-    assert deleted.undestroy!
-    assert_equal artist.attributes, Artist.find_by_name('Chick Corea').attributes
-    assert_nil Artist::Deleted.find_by_name('Chick Corea')
+    assert_models_equal artist, deleted
   end
+
+  def test_deleted_model_should_be_able_to_undestroy
+    assert(deleted = Artist::Deleted.find_by_name('Robert Walter'))
+    assert_nil Artist.find_by_name('Robert Walter')
+
+    deleted.undestroy!
+
+    assert_models_equal deleted, Artist.find_by_name('Robert Walter')
+    assert_nil Artist::Deleted.find_by_name('Robert Walter')
+    assert deleted.frozen?
+  end
+
+  private
+
+    def assert_models_equal(a, b, message = "models weren't equal")
+      reject_attrs = %q(deleted_at, updated_at)
+      assert_equal \
+        a.attributes.reject{|k,v| reject_attrs.include? k}, 
+        b.attributes.reject{|k,v| reject_attrs.include? k},
+        message
+    end
 end
